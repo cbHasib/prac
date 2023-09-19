8f0f0b701a8e3f4995fe9d710891980bec7af1c2
Hasibul Hasan <hasibul.hasan2905@gmail.com>
2023-09-20 02:17:57 +0600

    update

---
+++ C Program/avgMarkCal.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+int main ()
+{
+	float m1,m2,m3;
+	float per,avg;
+	float total;
+	
+	printf(" enter the marks of m1 ");
+	scanf("%f", &m1);
+	printf(" enter the marks of m2 ");
+	scanf("%f", &m2);
+	printf("enter the marks of m3 ");
+	scanf("%f", &m3);
+	total= m1+m2+m3;
+	printf("\n the total mark is %.2f ",total);
+	per = (total/300)*100;
+	avg = (total/3);
+	printf("\n the percentage mark is : %.2f ", per);
+	printf("\n the average mark  is: %.2f", avg);
+	
+	///// Grade calculation //
+	if(per>=80) printf("\n Grade : A");
+	else if(per>=60) printf("\n Grade : B");
+	else if(per>=40) printf("\n Grade : C");
+	else if(per<40) printf("\n Fail ");
+
+	return 0;
+}
